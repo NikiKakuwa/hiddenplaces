39cee5e2fd5c9b16de84a63f58afdfdb6a611801
Niki Kakuwa <haru_knk@yahoo.co.jp>
2020-05-26 18:29:38 +0900

    generate relationship controller, set relationsihp routes, description on User and Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,16 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
+
   validates :introduction, length: { maximum: 300 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'favorites/create'
   get 'favorites/destroy'
   devise_for :users
   get 'posts/about' => "posts#about"
-  resources :posts
-  resources :users
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :posts do
     resources :comments , only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
